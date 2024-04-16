c42d4623d5ae3be619dd5d532df12c96a88759e7
Rogério_Jr <135075315+Kitotsui@users.noreply.github.com>
2024-04-16 15:11:47 -0300

    Add files via upload

---
+++ Academia.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto01;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ *
+ * @author CAUPT - ALUNOS
+ */
+public class Academia {
+
+    private static long serial;
+    private long id;
+    private String nome;
+    private String endereco;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
+
+    public Academia()
+    {
+        id = Academia.serial++;
+    }
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(LocalDateTime dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+    
+    public void atualizarDataModificacao() {
+        this.dataModificacao = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "==================INFORMACOES DA ACADEMIA================= " 
+                + "\nID: " + id 
+                + "\nNOME: " + nome 
+                + "\nENDERECO: " + endereco 
+                + "\nDATA DE CRIACAO: " + dataCriacao 
+                + "\nDATA DE MODIFICACAO: " + dataModificacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.nome);
+        hash = 23 * hash + Objects.hashCode(this.endereco);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Academia other = (Academia) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.endereco, other.endereco);
+    }        
+}
---
+++ Main.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CAUPT - ALUNOS
+ */
+public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+    AcademiaDAO academiaDAO = new AcademiaDAO();
+    boolean academiaCriada = false;
+
+    public Main() {
+
+        int opcaoUsuario = 6;
+
+        while (opcaoUsuario != 5) {
+            opcaoUsuario = this.recebeOpcaoUsuario();
+
+            switch (opcaoUsuario) {
+                case 1:
+                    System.out.println("ACADEMIA CRIADA COM SUCESSO!");
+                    academiaCriada = true;
+                    break;
+                case 2:
+                     if (academiaCriada) {
+                        mostrarAcademia();
+                    } else {
+                        System.out.println("NENHUMA ACADEMIA CRIADA AINDA.");
+                    }
+                    break;
+                case 3:
+                    if (academiaCriada) {
+                        excluirAcademia();
+                        academiaCriada = false;
+                    } else {
+                        System.out.println("NENHUMA ACADEMIA CRIADA AINDA.");
+                    }
+                    break;
+                case 4:
+                    if (academiaCriada) {
+                        alterarNomeAcademia();
+                    } else {
+                        System.out.println("NENHUMA ACADEMIA CRIADA AINDA.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("FINALIZANDO O PROGRAMA...OBRIGADO POR UTILIZAR.");
+                    break;
+                default:
+                    System.out.println("escola uma opcao valida");
+                    break;
+            }
+        }
+        System.out.println("ATE A PROXIMA!");
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private int recebeOpcaoUsuario() {
+
+        StringBuilder builder = new StringBuilder("");
+
+        builder.append("SEJA BEM VINDO AO MEU PROGRAMA\n\n");
+        builder.append("\n1 - CRIAR ACADEMIA");
+        builder.append("\n2 - MOSTRAR ACADEMIA");
+        builder.append("\n3 - EXCLUIR ACADEMIA");
+        builder.append("\n4 - ALTERAR ACADEMIA");
+        builder.append("\n5 - SAIR DO PROGRAMA\n");
+        builder.append("\nQUAL A SUA OPCAO? \nR: ");
+
+        System.out.print(builder.toString());
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+ 
+
+    private void mostrarAcademia() {
+        academiaDAO.mostrarAcademia();
+    }
+
+    private void excluirAcademia() {
+        academiaDAO.excluirAcademia();
+         academiaCriada = false;
+        System.out.println("ACADEMIA EXCLUIDA COM SUCESSO!");
+    }
+
+    private void alterarNomeAcademia() {
+        System.out.println("DIGITE O NOVO NOME DA ACADEMIA: ");
+        String novoNome = scanner.nextLine();
+        academiaDAO.alterarNomeAcademia(novoNome);
+        System.out.println("NOME DA ACADEMIA ALTERADO COM SUCESSO!");
+    }
+
+}
