9ad199c3a24df83c3a0e420d1ffd11937365b389
Rogério_Jr <135075315+Kitotsui@users.noreply.github.com>
2024-05-22 11:27:49 -0300

    Update AcademiaController.java

--- Academia-POO/projetoAcademia/src/mvcAcademia/view/AcademiaController.java
+++ Academia-POO/projetoAcademia/src/mvcAcademia/view/AcademiaController.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  */
 public class AcademiaController {
 
-   private Scanner scanner;
+    private Scanner scanner;
 
     public AcademiaController() {
         this.scanner = new Scanner(System.in);
@@ -30,24 +30,22 @@ public class AcademiaController {
 
     public int exibirMenuCruds() {
         System.out.println("\n=============== MENU GERAL ================");
-        System.out.println("1 - CRUD ACADEMIA");
-        System.out.println("2 - CRUD PESSOA");
-        System.out.println("3 - CRUD EXERCICIO");
-        System.out.println("4 - CRUD EXERCICIO APLICACAO");
-        System.out.println("5 - CRUD DIVISAO DE TREINO");
-        System.out.println("6 - CRUD DIVISAO TREINO MUSCULO");
-        System.out.println("7 - CRUD TREINO");
-        System.out.println("8 - CRUD AVALIACAO FISICA");
-        System.out.println("9 - CRUD MENSALIDADE VIGENTE");
-        System.out.println("10 - GERAR PAGAMENTO MENSALIDADE");
-        System.out.println("11 - CRUD PAGAMENTO RECORRENTE");
-        System.out.println("12 - CRUD ENTRADA ALUNO");//a verificacao sera feita no login
-        System.out.println("13 - CRUD MOVIMENTACAO FINANCEIRA");
+        System.out.println("1 - ACADEMIAS");
+        System.out.println("2 - PESSOAS");
+        System.out.println("3 - EXERCICIOS");
+        System.out.println("4 - EXERCICIOS & APLICACOES");
+        System.out.println("5 - DIVISOES DE TREINO");
+        System.out.println("6 - DIVISOES DE TREINO MUSCULO");
+        System.out.println("7 - TREINOS");
+        System.out.println("8 - FICHA DE TREINO");
+        System.out.println("9 - AVALIACAO FISICA");
+        System.out.println("10 - MENSALIDADE VIGENTE");
+        System.out.println("11 - GERAR PAGAMENTO MENSALIDADE");
+        System.out.println("12 - PAGAMENTO RECORRENTE");
+        System.out.println("13 - MOVIMENTACOES FINANCEIRAS");
         System.out.println("14 - RELATORIO DE ALUNOS ADIMPLENTES");
         System.out.println("15 - RELATORIO MOVIMENTACAO FINANCEIRA");
-        System.out.println("16 - FICHA DE TREINO DO ALUNO");
-        
-        System.out.println("17 - VOLTAR AO MENU ANTERIOR");
+        System.out.println("16 - VOLTAR AO MENU ANTERIOR");
         System.out.print("OPCAO...: ");
 
         return Integer.parseInt(scanner.nextLine());
@@ -113,7 +111,7 @@ public class AcademiaController {
 
         return Integer.parseInt(scanner.nextLine());
     }
-    
+
     public int menuCrudTreino() {
         System.out.println("\n=============== CRUD TREINO ================");
         System.out.println("1 - CRIAR TREINO");
@@ -125,25 +123,88 @@ public class AcademiaController {
 
         return Integer.parseInt(scanner.nextLine());
     }
-    
-    public int menuCrudExercicioAplicacao() {
-    System.out.println("\n=============== CRUD EXERCICIO APLICACAO ================");
-    System.out.println("1 - ADICIONAR EXERCICIO APLICACAO");
-    System.out.println("2 - MOSTRAR EXERCICIOS APLICACAO");
-    System.out.println("3 - ALTERAR EXERCICIO APLICACAO");
-    System.out.println("4 - REMOVER EXERCICIO APLICACAO");
-    System.out.println("5 - VOLTAR AO MENU ANTERIOR");
-    System.out.print("OPCAO...: ");
 
-    return Integer.parseInt(scanner.nextLine());
-}
-    
+    public int menuCrudTreinoAplicacao() {
+        System.out.println("\n=============== CRUD TREINO ================");
+        System.out.println("1 - CRIAR FICHA DE TREINO APLICACAO");
+        System.out.println("2 - MOSTRAR FICHA DE TREINO APLICACAO");
+        System.out.println("3 - ALTERAR FICHA DE TREINO APLICACAO");
+        System.out.println("4 - EXCLUIR FICHA DE TREINO APLICACAO");
+        System.out.println("5 - ASSOCIAR E MONTAR FICHA DE TREINO");
+        System.out.println("6 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int menuCrudExercicioAplicacao() {
+        System.out.println("\n=============== CRUD EXERCICIO APLICACAO ================");
+        System.out.println("1 - ADICIONAR EXERCICIO APLICACAO");
+        System.out.println("2 - MOSTRAR EXERCICIOS APLICACAO");
+        System.out.println("3 - ALTERAR EXERCICIO APLICACAO");
+        System.out.println("4 - REMOVER EXERCICIO APLICACAO");
+        System.out.println("5 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int menuCrudAvaliacaoFisica() {
+        System.out.println("\n=============== AVALIACAO FISICA ================");
+        System.out.println("1 - REALIZAR AVALIACAO FISICA");
+        System.out.println("2 - MOSTRAR AVALIACOES FISICAS");
+        System.out.println("3 - ATUALIZAR AVALIACAO FISICA");
+        System.out.println("4 - EXCLUIR AVALIACAO FISICA");
+        System.out.println("5 - ASSOCIAR AVALIACAO FISICA AA PESSOA");
+        System.out.println("6 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int menuCrudAlunoPagamentoMensalidade() {
+        System.out.println("\n=============== CRUD ALUNO PAGAMENTO MENSALIDADE ================");
+        System.out.println("1 - ADICIONAR PAGAMENTO DA MENSALIDADE DO ALUNO");
+        System.out.println("2 - MOSTRAR PAGAMENTO DA MENSALIDADE DO ALUNO");
+        System.out.println("3 - ALTERAR PAGAMENTO DA MENSALIDADE DO ALUNO");
+        System.out.println("4 - REMOVER PAGAMENTO DA MENSALIDADE DO ALUNO");
+        System.out.println("5 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
     public int menuCrudMensalidadeVigente() {
-        System.out.println("\n=============== CRUD MENSALIDADE VIGENTE ================");
-        System.out.println("1 - CRIAR MENSALIDADE VIGENTE");
+        System.out.println("\n=============== CRUD ALUNO MENSALIDADE VIGENTE ================");
+        System.out.println("1 - ADICIONAR MENSALIDADE VIGENTE");
         System.out.println("2 - MOSTRAR MENSALIDADE VIGENTE");
-        System.out.println("3 - ATUALIZAR MENSALIDADE VIGENTE");
-        System.out.println("4 - EXCLUIR MENSALIDADE VIGENTE");
+        System.out.println("3 - ALTERAR MENSALIDADE VIGENTE");
+        System.out.println("4 - REMOVER MENSALIDADE VIGENTE");
+        System.out.println("5 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int menuCrudPagamentoRecorrente() {
+        System.out.println("\n=============== PAGAMENTO RECORRENTE ================");
+        System.out.println("1 - CADASTRAR PAGAMENTO RECORRENTE");
+        System.out.println("2 - MOSTRAR PAGAMENTOS RECORRENTES");
+        System.out.println("3 - ATUALIZAR PAGAMENTOS RECORRENTES");
+        System.out.println("4 - EXCLUIR PAGAMENTOS RECORRENTES");
+        System.out.println("5 - ASSOCIAR PAGAMENTO RECORRENTE AA PESSOA");
+        System.out.println("6 - VOLTAR AO MENU ANTERIOR");
+        System.out.print("OPCAO...: ");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int menuCrudMovimentacaoFinanceira() {
+        System.out.println("\n=============== MOVIMENTACOES FINANCEIRAS ================");
+        System.out.println("1 - CADASTRAR MOVIMENTACAO FINANCEIRA");
+        System.out.println("2 - MOSTRAR MOVIMENTACOES FINANCEIRAS");
+        System.out.println("3 - ATUALIZAR MOVIMENTACOES FINANCEIRAS");
+        System.out.println("4 - EXCLUIR MOVIMENTACOES FINANCEIRAS");
         System.out.println("5 - VOLTAR AO MENU ANTERIOR");
         System.out.print("OPCAO...: ");
 
